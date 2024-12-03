c14e0c9331d91d8bcd4a385fed883f1e83704d44
Victor <victor.paltnetter@hotmail.com>
2024-12-03 18:12:52 -0300

    remoção de arquivos desnecessarios

--- teste.py
+++
@@ -1,9 +0,0 @@
-upos = '(123,134)'
-upos9 = upos.split("(")
-print(upos9)
-upos0 = upos9[1].split(')')
-print(upos0)
-ipos = upos0[0].split(",")
-print(ipos)
-pos = (int(ipos[0]), int(ipos[1]))
-print(pos)
